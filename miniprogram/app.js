341fd302994d4556ea1647ffb1f4ad922932da2d
luyongkang <2726411438@qq.com>
2018-12-17 15:44:30 +0800

    第一次上传文件

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,15 +1,16 @@
 //app.js
 App({
-  onLaunch: function () {
+    onLaunch: function () {
     
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        traceUser: true,
-      })
-    }
+        if (!wx.cloud) {
+            console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+        } else {
+            wx.cloud.init({
+                env: 'development-f3b7be',
+                traceUser: true,
+            })
+        }
 
-    this.globalData = {}
-  }
+        this.globalData = {}
+    }
 })
