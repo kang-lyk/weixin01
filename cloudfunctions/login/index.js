2a01cfc10443126614f15ed253d7439d214d10a7
luyongkang <2726411438@qq.com>
2018-12-18 10:17:03 +0800

    增加了eslint

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -1,7 +1,7 @@
 // 云函数模板
 // 部署：在 cloud-functions/login 文件夹右击选择 “上传并部署”
 
-const cloud = require('wx-server-sdk')
+const cloud = require('wx-server-sdk');
 
 // 初始化 cloud
 cloud.init({
@@ -15,19 +15,19 @@ cloud.init({
  * 
  */
 exports.main = (event, context) => {
-  console.log(event)
-  console.log(context)
+    console.log(event)
+    console.log(context)
 
   // 可执行其他自定义逻辑
   // console.log 的内容可以在云开发云函数调用日志查看
 
   // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）
-  const wxContext = cloud.getWXContext()
+    const wxContext = cloud.getWXContext();
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+    return {
+        event,
+        openid: wxContext.OPENID,
+        appid: wxContext.APPID,
+        unionid: wxContext.UNIONID,
+    }
 }
